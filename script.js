60aa5e51517792d8127c7cdf3e3b170e4beda8f8
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-09-23 21:50:41 +0500

    First Class Function

---
+++ script.js
@@ -0,0 +1,26 @@
+//   ========================CallBack Function =================
+//  Asa function jo ki c function ko argument k tor par pass kia jay 
+
+function Message(){
+    console.log("Hello Everyone");
+    console.log("My Name is Fiza Rani");
+}
+
+ function greet(message){
+  message();
+ }
+ greet(Message);
+
+//   ================= Function 2 ===============
+ function message(a , b){ 
+    let sum = a + b;
+    return sum;
+ }
+
+ function result(Sum){
+   console.log(Sum(2 , 4));
+ } 
+
+ result(message);
+
+//   ===================== Function 3 ===============
