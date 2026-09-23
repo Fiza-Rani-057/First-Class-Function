eea0a7a56e4bdc039b0828075e91e5b317f42b26
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-09-23 22:08:08 +0500

    CallBack Function

--- script.js
+++ script.js
@@ -1,60 +1,69 @@
 //   ======================== CallBack Function =================
 //  Asa function jo ki c function ko argument k tor par pass kia jay 
 
-function Message(){
+function Message() {
     console.log("Hello Everyone");
     console.log("My Name is Fiza Rani");
 }
 
- function greet(message){
-  message();
- }
- greet(Message);
+function greet(message) {
+    message();
+}
+greet(Message);
 
 //   ================= Function 2 ===============
- function message(a , b){ 
+function message(a, b) {
     let sum = a + b;
     return sum;
- }
+}
 
- function result(Sum){
-   console.log(Sum(2 , 4));
- } 
+function result(Sum) {
+    console.log(Sum(2, 4));
+}
 
- result(message);
+result(message);
 
 //   ===================== Function 3 ===============
- function mess(){
+function mess() {
     console.log("Hello , I'm Fiza");
     console.log("I'm a Passionate Frontend Developer");
- }
+}
 
- function call(results){
-   results();
- }
- call(mess);
+function call(results) {
+    results();
+}
+call(mess);
 
 // ================= Higher Order Function =================
 // asa function jo kic dusre function ko argument me recieve kare 
 
 
- function Mess(){
-    console.log("I'm a Passionate Frontend Developer"); 
- }
- function messages(message){
-     message();  //Higher Order Function
- }
- messages(Mess);  //CallBack function
+function Mess() {
+    console.log("I'm a Passionate Frontend Developer");
+}
+function messages(message) {
+    message();  //Higher Order Function
+}
+messages(Mess);  //CallBack function
 
 //  ================= Clousers =====================
 
 //  jb iner function outer function k variable ko yad rky to wo 
 // clouser kehlata ha 
-function outer(){
+function outer() {
     let name = "Fiza";
-    function inner(){
+    function inner() {
         console.log(name);
     }
     inner();
 }
 outer();
+
+function Sum(a, b) {
+    let sum = a + b;
+    function calc() {
+        console.log(sum);
+    }
+    calc();
+}
+Sum( 2 , 5);
